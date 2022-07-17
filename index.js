c9fe614e66e34b2efa878966dc028def6f1407a5
hmrahim <h.m.rahimnet@gmail.com>
2022-07-17 08:04:57 +0600

    student view and edit api created succesfully

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 require("dotenv").config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion ,ObjectId} = require('mongodb');
 const port = process.env.PORT || 5000
 
 app.use(cors())
@@ -29,6 +29,42 @@ const run = async()=> {
             const result =await studentCollection.insertOne(body)
             res.send(result)
             console.log(result);
+        })
+
+        app.get("/addstudent",async(req,res)=> {
+            const data =await studentCollection.find().toArray()
+            res.send(data)
+        })
+
+        app.get("/addstudent/:id",async(req,res)=> {
+            const id = req.params.id
+            const query = {_id:ObjectId(id)}
+            const data = await studentCollection.findOne(query)
+            res.send(data)
+        })
+
+
+        app.patch("/addstudent/:id",async(req,res)=> {
+            const id = req.params.id
+            const data = req.body
+            const query = {_id:ObjectId(id)}
+            const doc = {
+                $set:{
+                    name:data.name,
+                    image:data.image,
+                    father:data.father,
+                    mother:data.mother,
+                    class:data.class,
+                    shift:data.shift,
+                    phone:data.phone,
+                    email:data.email,
+                    address:data.address,
+
+                }
+            }
+            const result = await studentCollection.updateOne(query,doc)
+
+            res.send(result)
         })
 
 
